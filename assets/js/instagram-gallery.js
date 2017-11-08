57081914352b02a37531fcd1b35187781f93eb30
urbanist-mathilde <urbanist-mathilde@gmail.com>
2017-11-08 22:39:08 +0100

    Backgrounds and fading words

--- assets/js/instagram-gallery.js
+++ assets/js/instagram-gallery.js
@@ -18,6 +18,21 @@ $(document).ready(function() {
 
 });
 
+var terms = $("ul li");
+
+	function rotateTerm() {
+
+	 var ct = $("#rotate").data("term") || 0;
+
+	// console.log(terms.eq([ct]).text());
+	 
+	  $("#rotate").data("term", 
+	  	ct == terms.length -1 ? 0 : ct + 1).text(terms.eq([ct]).text())
+	  .fadeIn().delay(2000).fadeOut(200, rotateTerm);
+
+	}
+	$(rotateTerm);
+
 //old
 //template: '<div class="col-lg-4 instaimg hvr-float"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 //
@@ -25,3 +40,19 @@ $(document).ready(function() {
 //        template: '<article><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="image"/></a><div class="caption"><h3>{{caption}}</h3></div></article>',
 
 //<article><a href="{{link}}"><img src="{{image}}" alt="{{caption}}" class="image"/></a><div class="caption">  <h3>{{caption}}</h3></div></article>
+
+
+// Change background depending on time of day
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 19 || n < 6)
+	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	else if (n > 16 && n < 19)
+	  // If time is between 4PM – 7PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
